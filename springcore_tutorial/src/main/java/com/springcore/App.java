27f6210b5936553114ebdb262b0dbac9734f6ff1
sakshipore <88977771+sakshipore@users.noreply.github.com>
2023-04-21 11:57:58 +0530

    Bean added

--- springcore_tutorial/src/main/java/com/springcore/App.java
+++ springcore_tutorial/src/main/java/com/springcore/App.java
@@ -1,13 +1,16 @@
 package com.springcore;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("studentBean");
+
+        System.out.println(student);
     }
 }
